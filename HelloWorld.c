8a9eeff1cbd8a7c37daec9ee1497327900159b4f
Nicky Grigg <griggnicky@gmail.com>
2022-10-27 09:17:18 -0400

    updated HelloWorld.c

--- HelloWorld.c
+++ HelloWorld.c
@@ -5,10 +5,7 @@
 // make sure to put the .c ^
 
 int main(void) {
-    int i;
-    printf("here \n");
-    scanf("%d", &i); // this was done to test to see what allows for a scanf to work
-    printf("%d \n", i); // I couldn't get the scanf to work using coderunner
+    printf("Hello World");
     
     return 0;
 }
