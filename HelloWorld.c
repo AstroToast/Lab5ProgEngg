104e61a9fe2f020e478d4356231e6e886e70a15a
Nicky Grigg <griggnicky@gmail.com>
2022-10-27 09:12:08 -0400

    added HelloWorld.c file

---
+++ HelloWorld.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+// cd  /Users/NickyG./Desktop/IntroProgrammingForEngineers/ 
+// ^ locates the director it will check and write to
+// gcc -o runme HelloWorld.c
+// make sure to put the .c ^
+
+int main(void) {
+    int i;
+    printf("here \n");
+    scanf("%d", &i); // this was done to test to see what allows for a scanf to work
+    printf("%d \n", i); // I couldn't get the scanf to work using coderunner
+    
+    return 0;
+}
